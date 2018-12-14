1d6cabbf3f1ac13d72a0eeb2585e82fd79faac72
Ha <ahanamzn@f0189889637b.ant.amazon.com>
2018-12-15 00:32:25 +1100

    finished day 2 challenges

--- day-1/part2.py
+++ day-1/part2.py
@@ -1,19 +1,37 @@
 #! /usr/bin/python
-# add input of integers together
-def findCorrectBox(intArray):
+# find the first double occurance when adding the line to the total
+def firstDouble(intArray):
 
-    return checkSum
+    total = 0
+    lineNo = 0
+    totalDict = {
+        0: 0
+    }
+
+    while True:
+        total += intArray[lineNo]
+        if total in totalDict:
+            break
+        else:
+            totalDict[total] = lineNo
+        lineNo = (lineNo + 1) % len(intArray)
+
+    return total
 
 def main():
 
-    assert(findCorrectBox(["abcdef", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]) == "fgij")
+    assert(firstDouble([1, -1]) == 0)
+    assert(firstDouble([3, 3, 4, -2, -4]) == 10)
+    assert(firstDouble([1, -2, 3, 1, 1, -2]) == 2)
+    assert(firstDouble([-6, 3, 8, 5, -6]) == 5)
+    assert(firstDouble([7, 7, -2, -7, -4]) == 14)
 
     fileInput = open('input1.txt','r')
     inputArray = []
     for line in fileInput:
-        inputArray.append(line)
-    checkSum = findCorrectBox(inputArray)
-    print(checkSum)
-  
+        inputArray.append(int(line))
+    fileTotal = firstDouble(inputArray)
+    print(fileTotal)
+
 if __name__== "__main__":
     main()
--- day-2/part2.py
+++ day-2/part2.py
@@ -1,37 +1,53 @@
 #! /usr/bin/python
-# find the first double occurance when adding the line to the total
-def firstDouble(intArray):
+# find the correct box as stated in advent calendar
+def findCorrectBox(intArray):
 
-    total = 0
-    lineNo = 0
-    totalDict = {
-        0: 0
-    }
+    noMaxMatches = 0
+    correctBox1 = 0
+    correctBox2 = 0
 
-    while True:
-        total += intArray[lineNo]
-        if total in totalDict:
-            break
-        else:
-            totalDict[total] = lineNo
-        lineNo = (lineNo + 1) % len(intArray)
+    # finding which pair has the most matches
+    for x in range(0, len(intArray)):
+        word1 = list(intArray[x])
+        for y in range(x+1, len(intArray)):
+            word2 = list(intArray[y])
+            matches = 0
+            # for lines of unequal length
+            if len(word1) < len(word2):
+                maxRange = len(word1)
+            else:
+                maxRange = len(word2)
+            for z in range (0, maxRange):
+                if word1[z] == word2[z]:
+                    matches += 1
+            if matches > noMaxMatches:
+                correctBox1 = x
+                correctBox2 = y
+                noMaxMatches = matches
 
-    return total
+    # displaying matches in nice fore
+    finalWord = []
+
+    word1 = list(intArray[correctBox1])
+    word2 = list(intArray[correctBox2])
+    for z in range (0, len(word1)):
+        if word1[z] == word2[z]:
+            finalWord.append(word1[z])
+
+    finalString = ''.join(finalWord)
+
+    return finalString
 
 def main():
 
-    assert(firstDouble([1, -1]) == 0)
-    assert(firstDouble([3, 3, 4, -2, -4]) == 10)
-    assert(firstDouble([1, -2, 3, 1, 1, -2]) == 2)
-    assert(firstDouble([-6, 3, 8, 5, -6]) == 5)
-    assert(firstDouble([7, 7, -2, -7, -4]) == 14)
+    assert(findCorrectBox(["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]) == "fgij")
 
     fileInput = open('input1.txt','r')
     inputArray = []
     for line in fileInput:
-        inputArray.append(int(line))
-    fileTotal = firstDouble(inputArray)
-    print(fileTotal)
-
+        inputArray.append(line)
+    checkSum = findCorrectBox(inputArray)
+    print(checkSum)
+  
 if __name__== "__main__":
     main()
